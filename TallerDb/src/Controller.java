c789c0ef0794b2e303b379e1ea778e47f997ea92
Juan Sebastian Muñoz <sebastian.jsmopu@gmail.com>
2023-02-23 22:02:37 -0500

    Lectura del archivo txt.

--- TallerDb/src/Controller.java
+++ TallerDb/src/Controller.java
@@ -1,8 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Controller {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
+	
+		System.out.println(System.getProperty("user.dir"));
 		
-	}
+		//ruta del archivo
+		Scanner sc = new Scanner(new File(System.getProperty("user.dir")+ "\\src\\archivo-1.txt"));
+	
+		while (sc.hasNextLine()) {
+            System.out.println(sc.nextLine());
+        }
 
+	}
 }
+	
+
